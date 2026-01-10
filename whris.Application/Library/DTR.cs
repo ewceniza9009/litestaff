ec3972297c35046e3481f954ebc2c2a0d4288ebf
web3lottery143-cloud <158169448+FlourenceL@users.noreply.github.com>
2026-01-10 15:49:47 +0800

    fix: negative tardy undertime hours

--- whris.Application/Library/DTR.cs
+++ whris.Application/Library/DTR.cs
@@ -1016,7 +1016,7 @@ namespace whris.Application.Library
   
         public static decimal ComputeTardyUndertimeHours(TrnDtrline line, IEnumerable<MstShiftCodeDay> shiftCodeDays, HRISContext _context) 
         {
-            if (DateOnly.FromDateTime(line.Date) == DateOnly.Parse("05/03/2025"))
+            if (DateOnly.FromDateTime(line.Date) == DateOnly.Parse("12/31/2025"))
             {
 
             }
@@ -1122,6 +1122,10 @@ namespace whris.Application.Library
                     }
 
                     numberOfHours = shiftNumberOfHours - (actualNumberOfHours - deductHours);
+                    if(numberOfHours < 0)
+                    {
+                        numberOfHours = 0;
+                    }
                 }
                 else 
                 {
